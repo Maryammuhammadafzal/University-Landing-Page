a0d95c5db8474f737deccc571dd318ec0d3a9633
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-07-21 15:16:10 +0500

    created team responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,16 @@ import Image from "next/image";
 import ContactPage from "./contact/page";
 import QuickActionPage from "./QuickAction/page";
 import TestimonialPage from "./testimonial/page";
+import TeamPage from "./team/page";
 
 export default function Home() {
   return (
     <div className="w-full h-auto overflow-hidden">
+<TeamPage/>
 <TestimonialPage/>
 <QuickActionPage/>
 <ContactPage/>
+
     </div>
   );
 }
--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -1,8 +1,9 @@
 import Heading from '@/components/Heading'
-import { TestimonialCarousel } from '@/components/TestimonialCarousel'
+import TeamCard from '@/components/TeamCard'
+
 import React from 'react'
 
-const TestimonialPage = () => {
+const TeamPage = () => {
     return (
         <div className='w-full h-auto flex flex-col justify-center lg:py-20 md:py-14 gap-10 sm:py-8 py-4 md:items-center'>
             <div className=' max-w-2xl items-center text-center h-auto flex flex-col gap-3 xl:pl-13 lg:pl-14 md:pl-10 px-5 justify-center'>
@@ -12,11 +13,11 @@ const TestimonialPage = () => {
                 </p>
 
             </div>
-            <div className=' w-full h-auto flex justify-center items-center'>
-                <TestimonialCarousel />
+            <div className=' w-full h-auto flex justify-center gap-5 items-center'>
+        <TeamCard/>
             </div>
         </div>
     )
 }
 
-export default TestimonialPage
+export default TeamPage
