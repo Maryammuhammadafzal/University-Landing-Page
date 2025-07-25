a36dc34fa5134a2538ae0c2f85d07a58d03fe91a
Maryammuhammadafzal <145895013+Maryammuhammadafzal@users.noreply.github.com>
2025-07-25 14:54:04 +0500

    Update TestimonialCarousel.tsx

--- src/components/TestimonialCarousel.tsx
+++ src/components/TestimonialCarousel.tsx
@@ -18,8 +18,8 @@ export function TestimonialCarousel() {
                         <div className="p-1 w-full flex h-full flex-col justify-center items-center relative text-center gap-8">
                             <p className="text-white lg:text-3xl md:text-2xl sm:text-xl xs:text-lg text-base font-semibold md:w-[80%] w-[98%]" >"Professor Khan not only taught me how to solve problems but also how to think differently — he changed my life!"</p>
                             <p className="text-white lg:text-3xl md:text-2xl sm:text-xl xs:text-lg text-sm font-semibold">– Ayesha R., Computer Science Graduate</p>
-                        <CarouselPrevious className="absolute left-0 md:top-37 sm:top-40 xs:top-40 top-43 " />
-                        <CarouselNext className="absolute md:top-37 sm:top-40 xs:top-40 top-43  right-5" />
+                        <CarouselPrevious className="absolute left-0 md:top-37 sm:top-40 xs:top-40 top-35 " />
+                        <CarouselNext className="absolute md:top-37 sm:top-40 xs:top-40 top-35  right-5" />
                         </div>
                     </CarouselItem>
                 ))}
